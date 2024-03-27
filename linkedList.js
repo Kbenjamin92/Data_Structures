c44c832003b89c365a47f332e104b3de984b0fe8
Kbenjamin92 <kipplbenjamin@gmail.com>
2024-03-27 10:36:31 -0400

    created addFirst method

--- linkedList.js
+++ linkedList.js
@@ -4,21 +4,37 @@
 // LinkedList class with two Node = first and Node = last
 // Add these methods... addFirst, addLast, deleteFirst, deleteLast, contains, indexOf
 
+class MyNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 class MyLinkedList {
     constructor() {
-        this.Node = head;
-        this.Node = tail;
+        this.head = null;
+        this.tail = null;
     }
-    addFirst(value) {
-        // adding a node to the beginning of the linked list making it the head
-       let node = new MyNode()
-       node.value = value;
-       this.Node.head = node
-       node.Node = this.Node.tail
-
+    // adding a node to the beginning of the linked list making it the head
+    addFirst(item) {
+       let node = new MyNode(item);
+        if (!this.head) {
+            // establishing the first node as the head
+            this.head = node;
+        } else {
+            // variable storing the node which the head is pointing too.
+            let current = this.head;
+            while (current.next) {
+                current = current.next
+            }
+            current.next = node;
+        }
     }
-    addLast() {
+    addLast(item) {
         // adding a node to the end of the linked lis making it the tail
+        let node = new MyNode();
+        node.value = item;
+        this.tail = node;
     }
     deleteFirst() {
         // deleting the first item of the linked list
@@ -34,13 +50,10 @@ class MyLinkedList {
     }
 }
 
-class MyNode {
-    constructor() {
-        this.value = value;
-        this.Node = next;
-    }
-}
-
 const list = new MyLinkedList();
 list.addFirst(1);
+list.addFirst(3);
+list.addFirst(4);
+list.addLast(8)
+
 console.log(list)
